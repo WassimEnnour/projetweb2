ee1ba59b727f3e19c81592ec27089346c2203d28
Your Name <you@example.com>
2024-11-07 20:00:55 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CheesecakeComponent } from './cheesecakes/cheesecakes.component';
+import { PiesComponent } from './pies/pies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheesecakeComponent,
+    PiesComponent
   ],
   imports: [
     BrowserModule
